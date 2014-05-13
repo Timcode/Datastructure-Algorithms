e4c2e30bf254d90f64eb48c24a0f68cf03186b61
Timon Brüllmann <tim_b00@hotmail.com>
2014-05-13 15:24:46 +0200

    Edmonds–Karp algorithm imll. however still contains bugs.

---
+++ Uebung_11/src/Main.java
@@ -0,0 +1,116 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		try {
+			int lines = sc.nextInt();
+
+			for (int j = 0; j < lines; j++) {
+				int n = sc.nextInt();
+				int m = sc.nextInt();
+
+				Graph g = new Graph(n, m);
+
+				for (int i = 0; i < m; i++) {
+					int u = sc.nextInt() - 1;
+					int v = sc.nextInt() - 1;
+					int cost = sc.nextInt();
+					g.addEdge(i, u, v, cost);
+				}
+
+				System.out.println(g.CalculateMaxFlow());
+
+			}
+
+		} finally {
+			sc.close();
+		}
+	}
+
+	private static class Graph {
+		private int n;
+		private int f = 0;
+		private int[][] capacity;
+		private int[][] currentFlow;
+		private LinkedList<Integer>[] neighbours;
+		private int m;
+		private int[] parentsOfCurrentPath;
+		private int[] capacityOfCurrentPath;
+
+		public Graph(int n, int m) {
+			this.n = n;
+			this.m = m;
+			neighbours = new LinkedList[n];
+			currentFlow = new int[n][];
+			capacity = new int[n][];
+			parentsOfCurrentPath = new int[n];
+			capacityOfCurrentPath = new int[n];
+
+			for (int i = 0; i < n; i++) {
+				neighbours[i] = new LinkedList<Integer>();
+				currentFlow[i] = new int[n];
+				capacity[i] = new int[n];
+			}
+		}
+
+		public void addEdge(int index, int u, int v, int c) {
+			capacity[u][v] = c;
+			neighbours[u].add(v);
+		}
+
+		public int CalculateMaxFlow() {
+			while (true) {
+				BreadthFirstSearch();
+
+				if (m == 0)
+					break;
+
+				f += m;
+
+				int v = n - 1;
+				while (v != 0) {
+					int u = parentsOfCurrentPath[v];
+					currentFlow[u][v] = currentFlow[u][v] + m;
+					currentFlow[v][u] = currentFlow[v][u] - m;
+					v = u;
+				}
+			}
+			return f;
+		}
+
+		private void BreadthFirstSearch() {
+			Arrays.fill(capacityOfCurrentPath, 0);
+			Arrays.fill(parentsOfCurrentPath, -1);
+			parentsOfCurrentPath[0] = -2;
+			capacityOfCurrentPath[0] = Integer.MAX_VALUE;
+			Queue<Integer> Q = new ArrayDeque<Integer>();
+			Q.add(0);
+			while (Q.size() > 0) {
+				int u = Q.remove();
+				for (Integer v : neighbours[u]) {
+					if (capacity[u][v] - currentFlow[u][v] > 0 && parentsOfCurrentPath[v] == -1) {
+						parentsOfCurrentPath[v] = u;
+						capacityOfCurrentPath[v] = Math.min(capacityOfCurrentPath[u], capacity[u][v] - currentFlow[u][v]);
+						if (v != n - 1) {
+							Q.add(v);
+						} else {
+							m = capacityOfCurrentPath[n - 1];
+							return;
+						}
+					}
+				}
+			}
+			m = 0;
+			return;
+		}
+
+	}
+	
+}
